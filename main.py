09ece63eff1f65aaf99c1314d736b5b563622594
Nikita Bushuev <nikita-1707@yandex.ru>
2024-06-10 22:41:13 +0300

    Your commit message

--- main.py
+++ main.py
@@ -288,4 +288,4 @@ async def delete_message(message_id: int, db: Session = Depends(get_db),
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
+    uvicorn.run("main:app", log_level="info", reload=True)
